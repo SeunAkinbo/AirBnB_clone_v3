6db6e8fb4286f5fe9aad5082f059cc93fa7615b0
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-04-27 20:06:34 +0100

    State: make_response

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """State module"""
-from flask import jsonify, request, abort, make_response
 from api.v1.views import app_views
+from flask import jsonify, request, abort, make_response
 from models import storage
 from models.state import State
 
