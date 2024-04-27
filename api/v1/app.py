3a54dd7fa6601345aa80311a941ba8fd15fa863e
brikhotso <brenda.rikhotso@gmail.com>
2024-04-26 17:47:16 -0700

    404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """AirBnB Clone API config file"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception):
     """Close the database connection after each request."""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
