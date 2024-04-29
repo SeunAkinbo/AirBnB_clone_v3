d30e6518ae6046e8b1623315df4490736c8bb9e6
brikhotso <brenda.rikhotso@gmail.com>
2024-04-29 03:37:01 -0700

    merge

---
+++ models/user.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        if kwargs and 'password' in kwargs:
+            password = kwargs.pop("password")
+            hasher = md5()
+            hasher.update(password.encode('utf-8'))
+            hashed_password = hasher.hexdigest()
+            kwargs['password'] = hashed_password
+        super().__init__(*args, **kwargs)
